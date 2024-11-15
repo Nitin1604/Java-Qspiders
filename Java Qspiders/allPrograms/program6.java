9d93c5957e616d7ea915800b9596b3f8a018c9ff
Nitin <nitinsokhal@gmail.com>
2024-11-15 17:55:05 +0530

    Java Q spiders Questions

---
+++ Java Qspiders/allPrograms/program6.java
@@ -0,0 +1,10 @@
+package allPrograms;
+
+public class program6 {
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = false;
+        System.out.println(!x);
+        System.out.println(x + " " + y);
+    }
+}
---
+++ Java Qspiders/allPrograms/program7.java
@@ -0,0 +1,10 @@
+package allPrograms;
+
+public class program7 {
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = false;
+        System.out.println(x && y);
+        System.out.println(x || y);
+    }
+}
