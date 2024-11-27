a1c932bdb3b7956598e3a44e75a55c5e7d4c9736
Nitin <nitinsokhal@gmail.com>
2024-11-27 17:41:38 +0530

    Java Q spiders Questions

---
+++ Java Qspiders/allPrograms/program24.java
@@ -0,0 +1,13 @@
+package allPrograms;
+
+public class program24 {
+    public static void main(String[] args) {
+        String s1 = "Mohan is here";
+        String s2 = s1.substring(7);
+        System.out.println(s2);
+
+        String s3 = s1.substring(3,10);
+        System.out.println(s3);
+
+    }
+}
