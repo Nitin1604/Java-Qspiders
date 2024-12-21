81efc8eb07bbd613b53287b80c32d4731df218eb
Nitin <nitinsokhal@gmail.com>
2024-12-21 11:30:24 +0530

    Java Q spiders Questions

---
+++ Java Qspiders/allPrograms/java/incrementDecrement2.java
@@ -0,0 +1,13 @@
+class incrementDecrement2
+{
+	public static void main(String[] args)
+	{
+		int a=15;
+		a++;
+		int b=a++;
+		int c=++b;
+		System.out.println("a is: "+a);
+		System.out.println("b is: "+b);
+		System.out.println("c is: "+c);
+	}
+}
