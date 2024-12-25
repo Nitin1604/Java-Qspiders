9a4f4498deda07cdf3cf4fb064b3300f826ff7d1
Nitin <nitinsokhal@gmail.com>
2024-12-25 23:49:44 +0530

    Java Q spiders Questions

---
+++ Java Qspiders/allPrograms/java/java 12pm/charData3.java
@@ -0,0 +1,11 @@
+class charData3
+{
+	public static void main(String[] args)
+	{
+		System.out.println((int)' ');
+		System.out.println((int)'*');
+		System.out.println((int)'+');
+		System.out.println((int)':');
+	}
+}
+
